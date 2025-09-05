6a318413b517905ef266d6686538dac80b342427
Anna Clark <anna@myclarkfamily.org>
2025-09-05 14:10:55 -0600

    Question 1

--- GITingToKnowYou/Program.cs
+++ GITingToKnowYou/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// Question 1: What is your favorite programming language and why?
+// Anna: C# becuase that's the one I know
